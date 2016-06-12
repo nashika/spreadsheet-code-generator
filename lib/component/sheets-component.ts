beebfe9e65c4b64ce4fecc58437852befb5e708b
nashika <info@nashika.net>
2016-06-12 14:08:49 +0900

    implement sheet tree display

--- lib/component/sheets-component.ts
+++ lib/component/sheets-component.ts
@@ -1,12 +1,25 @@
 import Component from "vue-class-component";
+import _ = require("lodash");
 
 import {BaseComponent} from "./base-component";
 import {ISheet, ISheetMeta} from "./app-component";
 import {templateLoader} from "./template-loader";
 
+interface ITreeSheet {
+  sheet:ISheet,
+  level:number,
+}
+
 @Component({
   template: templateLoader("sheets"),
   props: ["currentSheet", "sheets", "sheetMetas"],
+  ready: SheetsComponent.prototype.watchSheets,
+  watch: {
+    "sheets": {
+      handler: SheetsComponent.prototype.watchSheets,
+      deep: true,
+    },
+  },
 })
 export class SheetsComponent extends BaseComponent {
 
@@ -14,11 +27,13 @@ export class SheetsComponent extends BaseComponent {
   sheets:ISheet[];
   sheetMetas:ISheetMeta[];
 
+  treeSheets:ITreeSheet[];
   newName:string;
 
-  data() {
+  data():any {
     return {
       newName: "",
+      treeSheets: [],
     }
   }
 
@@ -33,6 +48,23 @@ export class SheetsComponent extends BaseComponent {
 
   remove():void {
     this.$root.services.sheet.remove();
+  }
+
+  watchSheets():void {
+    console.log("change sheets");
+    this.treeSheets = this.treeSheetRecursive("root", 0);
+  }
+
+  treeSheetRecursive(parentSheetName:string, level:number):ITreeSheet[] {
+    let result:ITreeSheet[] = [];
+    _.forIn(this.sheets, (sheet:ISheet) => {
+      if (sheet.parent == parentSheetName)
+        result.push({sheet: sheet, level: level});
+    });
+    result = _.sortBy(result, "sheet.name");
+    return _.flatMap(result, (treeSheet:ITreeSheet) => {
+      return _.concat([treeSheet], this.treeSheetRecursive(treeSheet.sheet.name, level + 1));
+    });
   }
 
 }
