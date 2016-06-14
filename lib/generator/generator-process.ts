4f53023cf5965c409d9ef3ead798ce7c9c50f43c
nashika <info@nashika.net>
2016-06-15 07:04:33 +0900

    copy original version csv loading code

--- lib/generator/generator-process.ts
+++ lib/generator/generator-process.ts
@@ -25,14 +25,36 @@ export class GeneratorProcess {
   constructor(protected app:AppComponent) {}
 
   public main() {
-    let accessor:GeneratorAccessor = new GeneratorAccessor();
-    let sheetObjects:{[sheetName:string]:TGeneratorSheetObject} = {};
-    for (let sheetName of this.app.services.code.list()) {
-      sheetObjects[sheetName] = this.requireSheetObject(sheetName, accessor);
-      if (!sheetObjects[sheetName]) return;
+    try {
+      let accessor:GeneratorAccessor = new GeneratorAccessor();
+      let sheetObjects:{[sheetName:string]:TGeneratorSheetObject} = {};
+      for (let sheetName of this.app.services.code.list()) {
+        sheetObjects[sheetName] = this.requireSheetObject(sheetName, accessor);
+        if (!sheetObjects[sheetName]) return;
+      }
+      accessor._sheetObjects = sheetObjects;
+
+      /*
+      console.log('Load CSV files was started.');
+      let readRecordsMap:TReadRecordsMap = {};
+      let readText:string;
+      for (let definitionName of DefinitionRegistry.keys) {
+        for (let fileName of [definitionName, `multi-${definitionName}`]) {
+          let filePath:string = path.join(`${DefinitionRegistry.CSV_DIR}/${fileName}.csv`);
+          if (fs.existsSync(filePath)) {
+            console.log(`Loading ${filePath} ...`);
+            readText = fs.readFileSync(filePath, "utf-8");
+            readRecordsMap[fileName] = parse(readText, {columns: true});
+          }
+        }
+      }*/
+
+      //console.log(result.generate());
+    } catch (e) {
+      this.app.services.generator.developerToolQuestion();
+      alert(e.stack || e);
+      throw e;
     }
-    accessor._sheetObjects = sheetObjects;
-    //console.log(result.generate());
   }
 
   protected requireSheetObject(sheetName:string, accessor:GeneratorAccessor):TGeneratorSheetObject {
@@ -41,31 +63,21 @@ export class GeneratorProcess {
     if (originalRequire.cache[sheetCodePath])
       delete originalRequire.cache[sheetCodePath];
     let sheetCode:TGeneratorSheetCode;
-    try {
       sheetCode = originalRequire(sheetCodePath);
-    } catch (e) {
-      alert(`Require error. file="${sheetCodePath}"\n\n${e.stack}`);
-      this.app.services.generator.developerToolQuestion();
-      throw e;
-    }
     if (typeof sheetCode != "function") {
-      alert(`Sheet code "${sheetName}.js" exports type="${typeof sheetCode}" data.
+      throw new Error(`Sheet code "${sheetName}.js" exports type="${typeof sheetCode}" data.
 Sheet code expects export type="function".\n\n${this.sheetCodeExample}`);
-      return null;
     }
     let sheetObject:TGeneratorSheetObject;
     try {
       sheetObject = sheetCode(accessor);
     } catch (e) {
-      alert(`Sheet code "${sheetName}.js" initialize error.
+      throw new Error(`Sheet code "${sheetName}.js" initialize error.
 Sheet code expects export function, one argument and return object.\n\n${this.sheetCodeExample}`);
-      this.app.services.generator.developerToolQuestion();
-      throw e;
     }
     if (typeof sheetObject != "object") {
-      alert(`Sheet code require("${sheetName}.js")($) return type="${typeof sheetCode}".
+      throw new Error(`Sheet code require("${sheetName}.js")($) return type="${typeof sheetCode}".
 Expects return type="object".\n\n${this.sheetCodeExample}`);
-      return null;
     }
     return sheetObject;
   }
