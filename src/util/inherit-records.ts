14b477d68e4a7c225ad0c104ee04ca29bfaa5751
nashika <info@nashika.net>
2016-12-05 17:20:38 +0900

    fix applyInherits process

--- src/util/inherit-records.ts
+++ src/util/inherit-records.ts
@@ -5,9 +5,11 @@ export class InheritRecords {
 
   paths: string[];
   records: {[key: string]: any};
+  columns: IColumn[];
 
   constructor(records: any[], columns: IColumn[]) {
     this.records = {};
+    this.columns = columns;
     let flag = true;
     this.paths = _(columns)
       .filter((column: IColumn) => (column.data == "extends") ? flag = false : flag)
@@ -19,7 +21,17 @@ export class InheritRecords {
       let key: string = _(this.paths)
         .map(path => record[path])
         .join(".");
-      this.records[key] = _.cloneDeep(record);
+      let myClone = (obj: any) => {
+        let result: any = {};
+        _.each(obj, (value, key) => {
+          if (_.isObject(value))
+            result[key] = myClone(value);
+          else if (value)
+            result[key] = value;
+        });
+        return result;
+      };
+      this.records[key] = myClone(record);
     });
     delete this.records[_(this.paths).map(p => "*").join(".")];
     _.each(this.records, record => this.applyInherit(record));
@@ -49,7 +61,12 @@ export class InheritRecords {
     let parentInheritKey = this.padInheritKey(extendsStr);
     if (this.records[parentInheritKey]) {
       let parentRecord = this.applyInherit(this.records[parentInheritKey]);
-      _.defaults(record, parentRecord);
+      for (let column of this.columns) {
+        let key: string = column.data;
+        if (!_.has(record, key) && _.has(parentRecord, key)) {
+          _.set(record, key, _.get(parentRecord, key));
+        }
+      }
       delete record["extends"];
       return record;
     } else {
