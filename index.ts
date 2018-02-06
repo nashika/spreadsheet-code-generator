1475b5fa9ead7a89d516f661a17048c168be8fdd
nashika <info@nashika.net>
2018-02-07 07:37:04 +0900

    move main process index.ts -> src/main.ts

--- index.ts
+++ index.ts
@@ -1,26 +1,3 @@
-import electron = require("electron");
 
-let win: Electron.BrowserWindow;
 
-function createWindow() {
-  win = new electron.BrowserWindow({width: 1200, height: 800});
-  win.loadURL(`file://${__dirname}/index.html`);
-  //win.webContents.openDevTools();
-  win.on('closed', () => {
-    win = null;
-  });
-}
-
-electron.app.on('ready', createWindow);
-
-electron.app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    electron.app.quit();
-  }
-});
-
-electron.app.on('activate', () => {
-  if (win === null) {
-    createWindow();
-  }
-});
+import "./src/main";
