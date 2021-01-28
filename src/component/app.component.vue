2bab34b0d6e9407bdb8147535613c990f97da9cc
nashika <info@nashika.net>
2021-01-28 18:02:23 +0900

    remove hub

--- src/component/app.component.vue
+++ src/component/app.component.vue
@@ -1,15 +1,4 @@
 <template lang="pug">
-  .app-component
-    #main.d-flex
-      #menu-area(v-if="$hub.showMenu")
-        menu-component()
-        sheets-component(v-if="$hub.sheets")
-        search-component()
-        column-component(v-if="$hub.mode == 'data' && $hub.currentSheet && $hub.currentSheet.name != 'root'")
-      #main-area(:class="{'col-xs-9': $hub.showMenu, 'col-xs-12': !$hub.showMenu}")
-        #main-message(v-if="$hub.mode == 'data' && $hub.currentSheet && $hub.currentSheet.name == 'root'") root sheet can not have data, please select or create new sheet.
-        spreadsheet-component(v-if="$hub.mode == 'data'")
-        code-editor-component(v-if="$hub.mode == 'code'")
 </template>
 
 <style lang="scss">
