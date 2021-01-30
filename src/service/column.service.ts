b3e7705cc6ca6a3a101320dad8dabcaf7f6393e5
nashika <info@nashika.net>
2021-01-30 14:11:56 +0900

    remove code and data service

--- src/service/column.service.ts
+++ src/service/column.service.ts
@@ -1,17 +1,4 @@
-import _ = require("lodash");
-import {injectable} from "inversify";
-import Vue from "vue";
-
-import {HubService, IColumn} from "./hub.service";
-import {BaseHubService} from "./base-hub.service";
-
-@injectable()
 export class ColumnService extends BaseHubService {
-
-  constructor(protected hubService: HubService) {
-    super(hubService);
-  }
-
   add(index: number): void {
     let columns = this.$hub.currentSheet.columns;
     this.$hub.currentSheet.columns = _.concat(_.slice(columns, 0, index), [this.generateInitialEmptyColumn()], _.slice(columns, index));
@@ -58,55 +45,6 @@ export class ColumnService extends BaseHubService {
   freeze(index: number): void {
     this.$hub.currentSheet.freezeColumn = index;
     this.$hub.currentSheetMeta.modified = true;
-  }
-
-  loadColumns(columns: IColumn[]) {
-    _(columns).each(column => {
-      _.defaults(column, this.generateInitialEmptyColumn(99));
-    });
-  }
-
-  generateInitialColumns(sheetName: string, parentSheetName: string): IColumn[] {
-    let treeColumns: IColumn[] = this.generateInitialTreeColumns(sheetName, parentSheetName);
-    let extendsColumns: IColumn[] = [{
-      header: "Extends",
-      data: "extends",
-      type: "text",
-      width: 120,
-      required: false,
-      export: false,
-      tsType: "",
-    }];
-    let emptyColumns: IColumn[] = _.times(5, this.generateInitialEmptyColumn);
-    return _.concat(treeColumns, extendsColumns, emptyColumns);
-  }
-
-  protected generateInitialTreeColumns(sheetName: string, parentSheetName: string): IColumn[] {
-    if (sheetName == "root") return [];
-    let parentSheet = this.$hub.sheets[parentSheetName];
-    let sheetColumn: IColumn = {
-      header: _.startCase(sheetName),
-      data: _.camelCase(sheetName),
-      type: "text",
-      width: 120,
-      required: true,
-      export: true,
-      tsType: "",
-    };
-    return _.concat(this.generateInitialTreeColumns(parentSheet.name, parentSheet.parent), [sheetColumn])
-  }
-
-  protected generateInitialEmptyColumn(no: number = undefined): IColumn {
-    no = _.isUndefined(no) ? this.$hub.currentSheet.columns.length : no;
-    return {
-      header: `Col${no}`,
-      data: `data${no}`,
-      type: "text",
-      width: 80,
-      required: false,
-      export: true,
-      tsType: "",
-    };
   }
 
 }
