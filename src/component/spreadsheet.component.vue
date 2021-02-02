dea921c10dc122b34f0ce710bfafe953eb434a9f
nashika <info@nashika.net>
2021-02-02 18:03:41 +0900

    remove spreadsheet-component

--- src/component/spreadsheet.component.vue
+++
@@ -1,6 +0,0 @@
-<template lang="pug">
-  .spreadsheet-component(style="height:100%")
-    div#spreadsheet(style="height:100%")
-</template>
-
-<script lang="ts" src="./spreadsheet.component.ts"></script>
