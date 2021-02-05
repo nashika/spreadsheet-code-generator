0889035df1b45dfa5663b213a63f6c5da40f904d
nashika <info@nashika.net>
2021-02-05 14:12:32 +0900

    remove code-editor-component

--- src/component/code-editor.component.vue
+++
@@ -1,6 +0,0 @@
-<template lang="pug">
-  .code-editor-component(style="height:100%")
-    div#code-editor(style="height:100%")
-</template>
-
-<script lang="ts" src="./code-editor.component.ts"></script>
